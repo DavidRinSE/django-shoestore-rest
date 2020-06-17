5e80dd17ca40e9da6dd3da612f957e98499220fd
DavidRinSE <davidrichardsonin@gmail.com>
2020-06-17 14:02:57 -0400

    API works, and Joe grew up in the Savana.

--- shoestore/settings.py
+++ shoestore/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest'
+    'rest',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
